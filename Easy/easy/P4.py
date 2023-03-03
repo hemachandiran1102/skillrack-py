23c3bf94a4229857d38196c6226ecc58929f95fd
Tinku <tinku@localhost.localdomain>
2023-03-03 23:44:03 +0530

    3-3-23

--- Easy/easy/P4.py
+++ Easy/easy/P4.py
@@ -20,6 +20,11 @@ Hence average speed = 180/5 = 36.00 kmph """
 
 
 
+from itertools import count
+from re import S
+from markupsafe import string
+
+
 def avgspeed():
     L="60@2 120@3"
     list=["60@2",'120@3']
@@ -32,8 +37,115 @@ def avgspeed():
         timet+=int(timetaken)
     print(totald//timet)
 
+#avgspeed()
+#-------------------------------------------------P2-------------------------------------------------- 
+"""
+INPUT:
+abcdefgxyzlmnoopqi
+5
+OUTPUT:
+ezo
+"""
+
+def chx():
+
+    string="abcdexyzwqpoolj"
+    
+    x=5
+    for i in range(1,len(string)):
+        if (i+1)%x==0:
+            print(string[i],end="")
+#chx()
+
+#------------------------------------------------P3------------------------------------------
+
+"""First Repeating Character A string S is passed as the input. 
+S has at least one repeating character. The program must print the first repeating character"""
+
+"""Example Input/Output 1: 
+Input: abcdexyzbwqpoolj 
+Output: b"""
+
+def reptchar():
+    A="abcdexyzbwqpoolj"
+    b=''
+    for i in range(len(A)):
+        for j in range(i+1,len(A)):
+            if A[i] == A[j]:
+                b+=A[j]
+                break
+    print(b[0])
+        
+#reptchar()
+
+""" A string S is passed as the input. S has at least one repeating character. 
+The program must print the first repeating character C from the last. 
+
+Example Input/Output 1: 
+Input: abcdexyzbwqpoolj 
+Output: p"""
+
+def reversereptchar():
+    #A="abcdexyzbwqpooplj"
+    A="mnadsh2327 237$$!#"
+    B=A[::-1]
+    b=''
+    for i in range(len(A)):
+        for j in range(i+1,len(A)):
+            if B[i] == B[j]:
+                b+=B[j]
+                break
+    
+    print(b[0])
+#reversereptchar()
+
+#-----------------------------------------------------P4--------------------------------------------
+"""Common part in string values 
+Two string values S1 and S2 are passed as input. 
+The last portion of S1 will be the first portion of S2. 
+The program must print this common part in S1 and S2.
+Input: 
+
+mayday 
+daybreak 
+
+Output: 
+
+day
+
+"""
+def matchstring():
+    S1="mayday"
+    S2="daybreak"
+    
+    string=""
+    
+    for I in range(len(S1)):
+        count=0
+        
+        for J in range(len(S2)):
+            if (J+count) > len(S2)-1 or I+count > len(S1)-1:
+                break
+            
+            elif S1[I+count] == S2[J+count]:
+                string+=S2[J+count]
+                count+=1
+                
+                
+            else:
+                count+=1
+                
+
+                    
+            
+    print(string)
 
 
+        
+                
+            
+                
+                
+    #print(string,list11)
+matchstring()
 
-avgspeed()
- 
