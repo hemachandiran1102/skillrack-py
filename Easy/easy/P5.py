48eedb73d04be9473839e945768d3c68d660c39b
hemachandiran1102 <76897910+hemachandiran1102@users.noreply.github.com>
2023-03-14 21:03:59 +0530

    Update P5.py

--- Easy/easy/P5.py
+++ Easy/easy/P5.py
@@ -282,6 +282,7 @@ def checkprime(N):
         return False
     for i in range(2,(N//2)+1):
         if (N%i==0):
+            print(i,N)
             return False
     return True
 def nextprime(N):
@@ -292,10 +293,41 @@ def nextprime(N):
             break
         else:
             A+=1
-nextprime(N)
-        
+#nextprime(N)
+checkprime(N)
+
+#------------------------------------------------------P7------------------------------------------------------------
+"""Count the primes in a range. Two whole numbers N1 and N2 are passed as input. 
+The program must print the number of primes present between N1 and N2 
+(the range is inclusive of N1 and N2) 
+Input Format: First line will contain the value of the first number N1 
+Second line will contain the value of the second number N2 
+
+Output Format: First line will contain the count of prime numbers between N1 and N2 
+
+Sample Input/Output: 
+Example 1: 
+Input: 
+6142 
+6200 
+
+Output: 
+6 
+Explanation: The prime numbers within the range 6142 to 6200 are 
+6143, 6151, 6163, 6173, 6197, 6199 
+
+Example 2: Input: 
+38 
+70 
+Output: 
+7 
+Explanation: The prime numbers within the range 38 to 70 are 
+41, 43, 47, 53, 59, 61, 67"""        
             
-        
+def primecheck(N):
+    if N==0 or N==1:
+        return False
+    
             
 
         
